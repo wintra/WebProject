82151cef30af981bed392538ff80f3007ebd790b
JiUng <wldnd1102@naver.com>
2020-06-04 16:43:32 +0900

    Implement Main, Login, Join HTML Page

--- src/main/java/com/webproject/app/SampleController.java
+++ src/main/java/com/webproject/app/SampleController.java
@@ -1,17 +1,36 @@
 package com.webproject.app;
 
+import java.text.DateFormat;
+import java.util.Date;
+import java.util.Locale;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
  
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
- 
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 @Controller
-@RequestMapping(value = "/sample")
 public class SampleController {
- 
-    @RequestMapping(value = "/getSample")
-    public void getSample(HttpServletRequest request, HttpServletResponse reponse) throws Exception {        
-        System.out.println("getSample");
-    }
+	private static final Logger logger = LoggerFactory.getLogger(SampleController.class);
+	
+	
+	@RequestMapping(value = "home.do", method = RequestMethod.GET)
+	public String home(Locale locale, Model model) {
+		return "home";
+	}
+
+	@RequestMapping(value = "login.do", method = RequestMethod.GET)
+	public String login(Locale locale, Model model) {
+		return "login";
+	}
+	
+	@RequestMapping(value = "join.do", method = RequestMethod.GET)
+	public String join(Locale locale, Model model) {
+		return "join";
+	}
 }
